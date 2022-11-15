63c3065064bb1e4b19288c78254e05d957116b7a
Uchenna Ihieri <uchennaihieri@yahoo.com>
2022-11-15 13:38:11 +0100

    updated Newsletter additions

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import Input from '../elements/Input';
+import Toastify from 'toastify-js'
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebase'
+import "toastify-js/src/toastify.css"
+import "../../assets/styles.css";
 
 const propTypes = {
   ...SectionProps.types,
@@ -26,6 +31,71 @@ const Cta = ({
   ...props
 }) => {
 
+
+  const [email, setEmail] = useState('')
+
+
+  const handleEmailSubmit = async () => {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email.match(validRegex)) {
+
+      await addDoc(collection(db, "emails"), {
+        email: email
+      }).then(() => Toastify({
+        text: "You have been added successfully",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "left",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () { }
+      }).showToast()
+      ).catch(() =>
+        Toastify({
+          text: "Unable to add, please try again",
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "left",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          onClick: function () { }
+        }).showToast()
+      );
+
+    } else {
+      Toastify({
+        text: "Email incorrect, Please check again",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "left",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () { }
+      }).showToast()
+    }
+
+
+
+    setEmail('')
+  }
+
+
+
+
   const outerClasses = classNames(
     'cta section center-content-mobile reveal-from-bottom',
     topOuterDivider && 'has-top-divider',
@@ -42,6 +112,9 @@ const Cta = ({
     split && 'cta-split'
   );
 
+
+
+
   return (
     <section
       {...props}
@@ -56,11 +129,14 @@ const Cta = ({
               Subscribe to exploreafritech newsletter              </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
+            <div className='inputSide'>
+              <Input value={email} id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your email" onChange={e => setEmail(e.target.value)}>
+
+              </Input>
+              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg" onClick={handleEmailSubmit}>
+                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" />
               </svg>
-            </Input>
+            </div>
           </div>
         </div>
       </div>
